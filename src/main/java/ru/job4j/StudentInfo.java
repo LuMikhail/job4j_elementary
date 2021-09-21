2b652c3989e1b8bf236c4b6fe8dfb668cda03f3b
Home <1984milu@mail.ru>
2021-09-21 21:45:45 +0300

    3.4 Заголовок в файле java на githab [#243562]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,9 +1,8 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Mikhail");
         System.out.println("08.04.1984");
-
     }
 }
