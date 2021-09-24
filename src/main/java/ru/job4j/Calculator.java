7b81cb139d090091493cb6894bb842077b80194a
Home <1984milu@mail.ru>
2021-09-24 22:34:35 +0300

    3.4 Арифметические операции в файле java на githab [#243562]

--- src/main/java/ru/job4j/Calculator.java
+++ src/main/java/ru/job4j/Calculator.java
@@ -1,14 +1,17 @@
 package ru.job4j;
 
-
 public class Calculator {
-    public static void plus(int first, int second) {
-        int itog = first + second;
-        System.out.println(itog);
-    }
-            public static void main(String[] args) {
-                Calculator.plus(100, 500);
-            Calculator.plus(4, 2);
-            Calculator.plus(3, 5);
+    public static void main(String[] args) {
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int two = 2;
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int fourTimeTwo = four * two;
+        System.out.println(sixDivTwo);
+        System.out.println(fiveMinusTwo);
+        System.out.println(fourTimeTwo);
+
     }
 }
