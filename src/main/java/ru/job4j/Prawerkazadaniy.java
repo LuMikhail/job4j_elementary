b3dbe65a525e174479c651b7745b413c8bdd720d
Home <1984milu@mail.ru>
2021-09-24 22:20:42 +0300

    3.5 Аргументы в файле java на githab [#243562]

---
+++ src/main/java/ru/job4j/Prawerkazadaniy.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class Prawerkazadaniy {
+    public static void main(String[] args) {
+        int size = 10;
+        System.out.println(size);
+        size = 100;
+        System.out.println(size);
+        size = size - 5;
+        System.out.println(size);
+
+    }
+}
