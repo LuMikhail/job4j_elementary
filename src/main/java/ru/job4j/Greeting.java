9c203fcc7a1f24bea3b0d497eed384a667b7d720
Home <1984milu@mail.ru>
2021-09-25 20:02:52 +0300

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/Greeting.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        String separate = " ";
+        idea = "I like Java!" + separate + "But I am a newbie";
+        int ear = 2021;
+        String separate1 = " ";
+        String stlearn = idea + separate1  + ear;
+        System.out.println(stlearn);
+    }
+}
