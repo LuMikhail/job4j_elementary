8c55bc1e4ee35b83230648687325b81455df6fbb
Home <1984milu@mail.ru>
2021-09-26 16:10:18 +0300

    0. Что такое тестирование [#159431]

---
+++ src/test/java/ru/job4j/ConverterTest.java
@@ -0,0 +1,25 @@
+package ru.job4j;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140RblThen2Euro() {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void rubleToDollar() {
+        float dol = 140;
+        float expecteddol = 2.3333333f;
+        float outdol = Converter.rubleToDollar(dol);
+        float eps = 0.0001f;
+        Assert.assertEquals(expecteddol, outdol, eps);
+    }
+}
