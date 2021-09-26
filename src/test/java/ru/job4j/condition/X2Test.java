9e16124f3afd4c8992baf0440ddd9d0cce1843a8
Home <1984milu@mail.ru>
2021-09-26 23:06:43 +0300

    1.2. Входные данные в тесте [#297750]

---
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class X2Test {
+
+    @Test
+    public void whenA10B0C0X2Then40() {
+        /* Входные параметра. Их будет 4 для данного случая*/
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        /* ожидаемое значение. Это всегда одна переменная. */
+        int expected = 3;
+        /* вызов метода, который хотим проверить. */
+        /* Результат вычисления. Это всегда одна переменная. */
+        int rsl = X2.calc(a, b, c, x);
+        /* сравнение полученного значения с ожидаемым */
+        Assert.assertEquals(expected, rsl);
+    }
+
+}
