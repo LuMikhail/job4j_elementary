ecb6ac1d586510fd6447e276432aa10542ea7902
Home <1984milu@mail.ru>
2021-09-27 00:45:24 +0300

    3. Тесты для определения расстояния между точками. [#159459]

---
+++ src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,27 @@
+package ru.job4j.condition;
+
+public class Point {
+    public static double distanceOne(int x1, int y1, int x2, int y2) {
+        double rsl = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        return rsl;
+    }
+
+    public static double distanceTwo(int x1, int y1, int x2, int y2) {
+        double rsl = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        return rsl;
+    }
+
+    public static double distanceThree(int x1, int y1, int x2, int y2) {
+        double rsl = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        return rsl;
+    }
+    public static void main(String[] args) {
+        double result = Point.distanceOne(0, 0, 3, 0);
+        double result2 = Point.distanceTwo(0, 0, 4, 0);
+        double result3 = Point.distanceThree(1, 0, 2, 0);
+        System.out.println("расстояния между точками1 (0, 0) to (3, 0) " + result);
+        System.out.println("расстояния между точками2 (0, 0) to (4, 0) " + result2);
+        System.out.println("расстояния между точками3 (0, 0) to (1, 0) " + result3);
+    }
+
+}
---
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+
+    @Test
+    public void distanceOne() {
+            double expected = 3;
+            int x1 = 0;
+            int y1 = 0;
+            int x2 = 3;
+            int y2 = 0;
+            double out = Point.distanceOne(x1, y1, x2, y2);
+            Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void distanceTwo() {
+        double expected = 4;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 4;
+        int y2 = 0;
+        double out = Point.distanceTwo(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void distanceThree() {
+        double expected = 1;
+        int x1 = 1;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double out = Point.distanceThree(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+}
