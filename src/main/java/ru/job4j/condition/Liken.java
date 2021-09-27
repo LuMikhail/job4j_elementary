243c3774017432f339d29de622ceb82a5e0f2a57
Home <1984milu@mail.ru>
2021-09-27 23:41:06 +0300

    0. Операции сравнения [#235631]

--- src/main/java/ru/job4j/condition/Liken.java
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -5,10 +5,10 @@ public class Liken {
         int first = 10;
         int second = 9;
         boolean result = first > second; /* first больше second? */
+        boolean result2 = first < second; /* first меньше second? */
+        boolean result3 = first == second; /* first равна second? */
         System.out.println(result);
-        boolean result2 = first < second;/* first меньше second? */
         System.out.println(result2);
-        boolean result3 = first == second;/* first равна second? */
         System.out.println(result3);
     }
 }
