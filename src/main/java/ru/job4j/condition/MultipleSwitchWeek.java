549d6e8afeacc57738f7b7e09b7805331f1ea6c7
Home <1984milu@mail.ru>
2021-10-02 09:34:52 +0300

    4.6.2. Switch - matcher

--- src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
+++ src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
@@ -4,11 +4,13 @@ public class MultipleSwitchWeek {
     public static int numberOfDay(String name) {
         return switch (name) {
             case "Понедельник" -> 1;
-            default -> - 1;
+            case "Вторник" -> 2;
+            default -> 0;
         };
     }
 
         public static void main(String[] args) {
         System.out.println(numberOfDay("Понедельник"));
+            System.out.println(numberOfDay("Вторник"));
     }
 }
