4e162bd0c55c2c16b0f03a639ecf6691ecb52d95
Home <1984milu@mail.ru>
2021-10-06 13:54:09 +0300

    5.6. Простое число

--- src/main/java/ru/job4j/loop/PrimeNumber.java
+++ src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -6,7 +6,7 @@ public class PrimeNumber {
         for (int number = 1; number <= finish; number++) {
             if (CheckPrimeNumber.check(number)) {
                  count++;
-            }
+                }
         }
         return count;
     }
