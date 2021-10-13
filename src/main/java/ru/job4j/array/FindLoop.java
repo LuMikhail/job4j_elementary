389916b7c67e23e9d1e5e4be1622c0219d7131d1
Home <1984milu@mail.ru>
2021-10-13 14:06:50 +0300

    6.1. Классический поиск перебором.

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -7,7 +7,7 @@ public class FindLoop {
             if (data[index] == el) {
                 rst = index;
                 break;
-            }
+             }
         }
         return rst;
     }
