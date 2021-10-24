7a04193cc79d0175ea7f91592d7f9d7129304a86
Home <1984milu@mail.ru>
2021-10-24 06:56:44 +0300

    6.7.2. Моно столбец в матрице другой вариант

---
+++ src/test/java/ru/job4j/array/MatrixCheckAnotherTest.java
@@ -0,0 +1,66 @@
+package ru.job4j.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MatrixCheckAnotherTest {
+    @Test
+    public void whenHasMonoHorizontal() {
+        char[][] input = {
+                {' ', ' ', ' '},
+                {'X', 'X', 'X'},
+                {' ', ' ', ' '},
+        };
+        int row = 1;
+        boolean result = MatrixCheckAnother.monoHorizontal(input, row);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void whenHasMonoHorizontalFalse() {
+        char[][] input = {
+                {' ', ' ', ' '},
+                {'X', 'X', ' '},
+                {' ', ' ', ' '},
+        };
+        int row = 1;
+        boolean result = MatrixCheckAnother.monoHorizontal(input, row);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void whenHasMonoVertical() {
+        char[][] input = {
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'},
+        };
+        int column = 2;
+        boolean result = MatrixCheckAnother.monoVertical(input, column);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void whenHasMonoVerticalOne() {
+        char[][] input = {
+                {' ', 'X', ' '},
+                {' ', 'X', ' '},
+                {' ', 'X', ' '},
+        };
+        int column = 1;
+        boolean result = MatrixCheckAnother.monoVertical(input, column);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void whenHasMonoVerticalFalse() {
+        char[][] input = {
+                {' ', 'X', ' '},
+                {' ', ' ', ' '},
+                {' ', 'X', ' '},
+        };
+        int column = 1;
+        boolean result = MatrixCheckAnother.monoVertical(input, column);
+        Assert.assertFalse(result);
+    }
+}
